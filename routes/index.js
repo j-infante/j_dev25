aa391dc36563f269afba95db82bd2cf4f8a84d03
Josua <josua.infante@volenday.com>
2018-05-25 16:15:26 +0800

    telegram node

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const TelegramBot = require('node-telegram-bot-api');
+const token = '592619128:AAFM798mn1cJdw-XN2KTnEhdV8pakfjlFho';
+const bot = new TelegramBot(token, {polling: true});
 
+bot.on('message', (msg) => {
+  console.log('msg: ', msg);
+     if(msg.text){
+        bot.sendMessage(msg.chat.id, 
+          "Fuck off user")
+     }
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jdev_25 BOT API' });
 });
-
-router.get('/next',function(req,res,next){
-  res.render('index',{ title: "next route"})
-})
 
 module.exports = router;
